daa2d584a67017db5e0bda0e37f7b43315d612e6
Sahil <sahilbondre@gmail.com>
2019-12-19 18:14:57 +0530

    Allocate all the tokens to contract deployer

--- contracts/BrownieToken.sol
+++ contracts/BrownieToken.sol
@@ -3,7 +3,10 @@ pragma solidity ^0.5.0;
 contract BrownieToken{
   uint public totalSupply;
 
+  mapping (address => uint) public balanceOf;
+
   constructor() public {
     totalSupply = 1000;
+    balanceOf[msg.sender] = totalSupply;
   }
 }
--- test/BrownieToken.test.js
+++ test/BrownieToken.test.js
@@ -7,4 +7,13 @@ contract("BrownieToken", accounts => {
     const tokensSupply = await instance.totalSupply();
     assert.equal(tokensSupply, 1000);
   });
+
+  it("should  allocate all the tokens to contract deployer", async () => {
+    const instance = await BrownieToken.deployed();
+
+    const deployerBalance = await instance.balanceOf(accounts[0]);
+    const otherBalance = await instance.balanceOf(accounts[1]);
+    assert.equal(deployerBalance, 1000);
+    assert.equal(otherBalance, 0);
+  });
 });
