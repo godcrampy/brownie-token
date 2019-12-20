5aa82b86d4eb354ddd6d1c9867ba5cc3220cff81
Sahil <sahilbondre@gmail.com>
2019-12-20 15:10:09 +0530

    Add transfer token function

--- contracts/BrownieToken.sol
+++ contracts/BrownieToken.sol
@@ -6,9 +6,19 @@ contract BrownieToken{
   string public symbol = "BRWN";
   uint public decimals = 10;
 
+  event Transfer(address indexed _from, address indexed _to, uint256 _value);
+
   mapping (address => uint) public balanceOf;
 
   constructor() public {
     balanceOf[msg.sender] = totalSupply;
+  }
+
+  function transfer(address _to, uint _value) public returns (bool success) {
+    require(balanceOf[msg.sender] >= _value, "Inadequate Balance");
+    balanceOf[msg.sender] -= _value;
+    balanceOf[_to] += _value;
+    emit Transfer(msg.sender, _to, _value);
+    return true;
   }
 }
--- test/BrownieToken.test.js
+++ test/BrownieToken.test.js
@@ -1,33 +1,76 @@
 const BrownieToken = artifacts.require("./BrownieToken.sol");
 
 contract("BrownieToken", accounts => {
-  it("should set total supply of tokens to 1000", async () => {
-    const instance = await BrownieToken.deployed();
+  describe("Constructor", () => {
+    it("should set total supply of tokens to 1000", async () => {
+      const instance = await BrownieToken.deployed();
 
-    const tokensSupply = await instance.totalSupply();
+      const tokensSupply = await instance.totalSupply();
 
-    assert.equal(tokensSupply, 1000);
+      assert.equal(tokensSupply, 1000);
+    });
+
+    it("should allocate all the tokens to contract deployer", async () => {
+      const instance = await BrownieToken.deployed();
+
+      const deployerBalance = await instance.balanceOf(accounts[0]);
+      const otherBalance = await instance.balanceOf(accounts[1]);
+
+      assert.equal(deployerBalance, 1000);
+      assert.equal(otherBalance, 0);
+    });
+
+    it("should set name, symbol and decimals options", async () => {
+      const instance = await BrownieToken.deployed();
+
+      const name = await instance.name();
+      const symbol = await instance.symbol();
+      const decimals = await instance.decimals();
+
+      assert.equal(name, "Brownie Token");
+      assert.equal(symbol, "BRWN");
+      assert.equal(decimals, 10);
+    });
   });
 
-  it("should allocate all the tokens to contract deployer", async () => {
-    const instance = await BrownieToken.deployed();
+  describe("trasfer function", () => {
+    it("should throw error if tokens more than the balance are transfered", async () => {
+      const instance = await BrownieToken.deployed();
+      // const err = await instance.transfer(accounts[0], 10e5);
+      try {
+        await instance.transfer(accounts[0], 10e5);
+        assert.fail();
+      } catch (error) {
+        assert.include(error.message, "Inadequate Balance");
+      }
+    });
 
-    const deployerBalance = await instance.balanceOf(accounts[0]);
-    const otherBalance = await instance.balanceOf(accounts[1]);
+    it("should transfer token", async () => {
+      const instance = await BrownieToken.deployed();
 
-    assert.equal(deployerBalance, 1000);
-    assert.equal(otherBalance, 0);
-  });
+      await instance.transfer(accounts[1], 100, { from: accounts[0] });
+      const senderBalance = await instance.balanceOf(accounts[0]);
+      const recieverBalance = await instance.balanceOf(accounts[1]);
 
-  it("should set name, symbol and decimals options", async () => {
-    const instance = await BrownieToken.deployed();
+      assert.equal(senderBalance.toNumber(), 900);
+      assert.equal(recieverBalance.toNumber(), 100);
+    });
 
-    const name = await instance.name();
-    const symbol = await instance.symbol();
-    const decimals = await instance.decimals();
+    it("should emit 'Transfer' event", async () => {
+      const instance = await BrownieToken.deployed();
 
-    assert.equal(name, "Brownie Token");
-    assert.equal(symbol, "BRWN");
-    assert.equal(decimals, 10);
+      const reciept = await instance.transfer(accounts[1], 100, { from: accounts[0] });
+
+      assert.equal(reciept.logs[0].event, "Transfer");
+      assert.equal(reciept.logs[0].args._from, accounts[0]);
+      assert.equal(reciept.logs[0].args._to, accounts[1]);
+      assert.equal(reciept.logs[0].args._value, 100);
+    });
+
+    it("should return boolean value", async () => {
+      const instance = await BrownieToken.deployed();
+      const result = await instance.transfer.call(accounts[0], 100, { from: accounts[1] });
+      assert.equal(result, true);
+    });
   });
 });
